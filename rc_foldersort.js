decff21aaa62d04ea66be840e6579c9e54e7bc66
Sandor Takacs <sandor.takacs@iwd.hu>
2014-03-03 17:06:07 +0100

    More debug

--- rc_foldersort.js
+++ rc_foldersort.js
@@ -68,5 +68,5 @@ rcmail.addEventListener('init', function() {
 });
 
 rcmail.addEventListener('responsebefore', function(resp) {
-    console.log('responsebefore resp: ' + resp);
+    console.log('responsebefore resp: task: ' + resp.task + 'action: ' + resp.action);
 });
