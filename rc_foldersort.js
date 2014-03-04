4a419b1697ffdeac5ebe49ffa3703bcc985e0c9f
Sandor Takacs <sandor.takacs@iwd.hu>
2014-03-04 17:00:05 +0100

    Sometimes beforelist called with full rcube object, not just the folder name

--- rc_foldersort.js
+++ rc_foldersort.js
@@ -85,7 +85,47 @@ if (window.rcmail) {
      * EventListener to change the sorting order before we list the messages
      */
     rcmail.addEventListener('beforelist', function(props) {
-        if (props && rcmail.task == 'mail') {
+        var folder = rcmail.env.mailbox;
+        if (props) {
+            if (typeof(props) == 'object' && props.ref == 'rcmail') {
+                folder = props.env.mailbox;
+            } else if (typeof(props) == 'string') {
+                folder = props;
+            }
+
+            if (rcmail.task == 'mail') {
+                var folder_sort;
+                orig_col   = rcmail.env.sort_col;
+                orig_order = rcmail.env.sort_order;
+                console.log('beforelist');
+                console.log(folder);
+                console.log('beforelist before folder: ' + folder + ', col: ' + orig_col + ', order: ' + orig_order);
+    
+                if (rcmail.env.per_folder_sort) {
+                    if (rcmail.env.per_folder_sort[folder]) {
+                        folder_sort = rcmail.env.per_folder_sort[folder];
+                    } else if (rcmail.env.per_folder_sort['default']) {
+                        folder_sort = rcmail.env.per_folder_sort['default'];
+                    } else {
+                        folder_sort = orig_col + '_' + orig_order;
+                    }
+    
+                    var y = folder_sort.split("_", 2);
+                    col   = y[0];
+                    order = y[1];
+                    if (orig_col != col || orig_order != order) {
+                        $('#rcm' + orig_col).removeClass('sorted' + (orig_order.toUpperCase()));
+                        $('#rcm' + col).addClass('sorted' + order);
+                        rcmail.env.sort_col   = col;
+                        rcmail.env.sort_order = order;
+                        // rcmail.list_mailbox(props, '', folder_sort);
+                        console.log('beforelist changed folder: ' + folder + ', col: ' + col + ', order: ' + order);
+                    }
+                }
+            }
+        }
+        /*
+        if (props && typeof(props) == 'string' && rcmail.task == 'mail') {
             var folder_sort;
             orig_col   = rcmail.env.sort_col;
             orig_order = rcmail.env.sort_order;
@@ -115,6 +155,7 @@ if (window.rcmail) {
                 }
             }
         }
+        */
     });
 
     /*
